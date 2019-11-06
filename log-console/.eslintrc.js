1723d4d146db4aacad50b4bfe145b79b409f88e7
zhanghao <506365426@qq.com>
2019-11-06 14:26:12 +0800

    chore : 构建工具配置

---
+++ log-console/.eslintrc.js
@@ -0,0 +1,30 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+	root: true,
+	parserOptions: {
+		parser: 'babel-eslint'
+	},
+	env: {
+		browser: true,
+	},
+	extends: [
+		// https://github.com/standard/standard/blob/master/docs/RULES-en.md
+		'standard'
+	],
+	// required to lint *.vue files
+	plugins: [],
+	// add your custom rules here
+	rules: {
+		'generator-star-spacing': 'off',
+		'semi': "off",
+		'no-debugger': "off",
+		'quotes': ["error", "single"],
+		'object-curly-spacing': ["error", "always"]
+	},
+	"globals": {
+		'alert': true,
+		'values': true,
+		'entries': true
+	}
+}
---
+++ log-console/webpack.config.js
@@ -0,0 +1,65 @@
+'user strict'
+/* eslint-disable */
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const lodashWebpackPlugin = require('lodash-webpack-plugin')
+const terserPlugin = require('terser-webpack-plugin')
+
+function resolve (dir) {
+	return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+	entry: {
+		'loader-api': './src/index.js',
+		'loader-api.min': './src/index.js'
+	},
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: '[name].js',
+		library: 'LogConsoleLibrary',	//指定库的全局变量
+		libraryExport: 'default',		//默认即使用LoaderApiLibrary为插件的全局变量名
+		libraryTarget: 'umd'			//支持库引入的方式 AMD、CJS、EM module、CDN
+	},
+	// mode: 'development',
+	mode: 'none',
+	resolve: {
+		extensions: ['.js', '.json'],
+		alias: {
+		  '@': resolve('src')
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|vue)$/,
+				loader: 'eslint-loader',
+				enforce: 'pre',
+				include: [resolve('src'), resolve('test')],
+				options: {
+					formatter: require('eslint-friendly-formatter')
+				}
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ['babel-loader'],
+				include: [resolve('src'), resolve('test')]
+			}
+		]
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new lodashWebpackPlugin()
+	],
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new terserPlugin({
+				include: /\.min\.js$/
+			})
+		]
+	},
+	devtool: 'source-map'
+}
+
