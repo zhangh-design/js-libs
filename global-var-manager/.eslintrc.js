fbd9d301b66eceac3a3c174e37ca197091017006
zhanghao <506365426@qq.com>
2019-11-07 18:00:17 +0800

    chore : 添加配置文件

---
+++ global-var-manager/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+	root: true,
+	parserOptions: {
+		parser: 'babel-eslint'
+	},
+	env: {
+		browser: true,
+	},
+	extends: [
+		// https://github.com/standard/standard/blob/master/docs/RULES-en.md
+		'standard'
+	],
+	// required to lint *.vue files
+	plugins: [],
+	rules: {
+		'semi': "off",
+		'no-debugger': "off",
+		'quotes': ["error", "single"],
+		'object-curly-spacing': ["error", "always"]
+	},
+	"globals": {
+		'alert': true,
+		'values': true,
+		'entries': true
+	}
+}
