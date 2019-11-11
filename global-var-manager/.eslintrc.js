aa74c0683aa734430d747a539935beee9d8a7a51
zhanghao <506365426@qq.com>
2019-11-11 16:53:36 +0800

    docs : no-template-curly-in-string、no-useless-concat启用 模板字面量 `` 设置为 on

--- global-var-manager/.eslintrc.js
+++ global-var-manager/.eslintrc.js
@@ -16,7 +16,9 @@ module.exports = {
 		'semi': "off",
 		'no-debugger': "off",
 		'quotes': ["error", "single"],
-		'object-curly-spacing': ["error", "always"]
+		'object-curly-spacing': ["error", "always"],
+		'no-template-curly-in-string': "on",
+		'no-useless-concat': "on"
 	},
 	"globals": {
 		'alert': true,
