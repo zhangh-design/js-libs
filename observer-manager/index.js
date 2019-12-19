7fdc89296c489d9017f3b1d30e8e783b10b39507
zhanghao <506365426@qq.com>
2019-12-19 11:38:41 +0800

    fix: 修改开发和生成环境下的dist目录对应文件

--- observer-manager/index.js
+++ observer-manager/index.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/log-console.min.js')
+  module.exports = require('./dist/observer-manager.min.js')
 } else {
-  module.exports = require('./dist/log-console.js')
+  module.exports = require('./dist/observer-manager.js')
 }
